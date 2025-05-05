369b773666d0df0dc24acb8f185184625959f84b
eme <emerarural@gmail.com>
2025-05-06 08:08:40 +0900

    🐈️ index.js編集 + index.html, style.css新規追加
    index.jsを編集→ res.sendFileにて、index.htmlを読み込む
    index.html, style.cssを新規登録

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});//app.get
+
 app.listen(8080, () =>{
 	console.log('🐈️ listening on localhost port 8080');
-});
+});//app.listen
