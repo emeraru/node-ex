a18c5ca649323ab0425c49cbdd15a01c4227dd86
eme <emerarural@gmail.com>
2025-05-08 21:46:20 +0900

    🐈️ post.htmlを新規追加
    post.htmlを新規に追加
    data.txtを新規に追加
    index.html, index.jsを編集

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
+
+app.use(express.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });//app.get
+
+app.post("/eme", (req, res) => {
+	console.log('🐓 post');
+	console.log(req.body.comment);
+	fs.writeFile(__dirname + "/data.txt", req.body.comment, () => {
+		res.sendFile(__dirname + "/post.html");
+	});
+});//app.post
 
 app.listen(8080, () =>{
 	console.log('🐈️ listening on localhost port 8080');
