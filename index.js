c3b0e28e53775bd1abf400cbe0144402a148a422
eme <emerarural@gmail.com>
2025-05-11 21:53:16 +0900

    🐈️ index.js, index.htmlを編集
    index.js, index.htmlを編集
    GitHubの学習を進めるためにとりあえずコミット

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+const act = require('./act.json');
 
 app.use(express.urlencoded({extended:true}));
 
@@ -8,12 +9,25 @@ app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });//app.get
 
+app.get("/act", (req, res) => {
+	res.send(act);
+});
+
 app.post("/eme", (req, res) => {
 	console.log('🐓 post');
 	console.log(req.body.comment);
 	fs.writeFile(__dirname + "/data.txt", req.body.comment, () => {
 		res.sendFile(__dirname + "/post.html");
 	});
+});//app.post
+
+app.post("/update", (req, res) => {
+	act[0].act = req.body.new_act;
+	res.send(act);
+});//app.post
+
+app.post("/del", (req, res) => {
+	console.log(req.body);
 });//app.post
 
 app.listen(8080, () =>{
